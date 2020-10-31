0b859c8f895c53d55527e1f452fb39ad61fe4a48
ns3098 <ns3098@bennett.edu.in>
2020-10-31 15:38:18 +0530

    update settings

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -26,10 +26,10 @@ if os.path.isfile(dotenv_file):
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "977ba28c395ca242c285fb997243c8379ae5496c5a6a9845"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get("DEBUG_VALUE") == "True")
 
 ALLOWED_HOSTS = ['online-book-inventory.herokuapp.com','127.0.0.1']
 
